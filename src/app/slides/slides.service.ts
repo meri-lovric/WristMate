b3a43a9b3bc85c09b11900ddc3f6e154f322c2a8
meri-lovric <meri.lovric1@gmail.com>
2021-07-29 18:49:22 +0200

    Slides

--- src/app/slides/slides.service.ts
+++ src/app/slides/slides.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Slide } from './slide.model';
-
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class SlidesService {
+  connectedDevicesArray: Array<Object> = []
+  private messageSource = new BehaviorSubject([]);
+  currentMessage = this.messageSource.asObservable();
+
   private slides: Slide[] = [
     {
       id: 'slide1',
@@ -35,5 +39,9 @@ export class SlidesService {
   constructor() {}
   getAllSlides() {
     return [...this.slides];
+  }
+  changeMessage(message: Object){
+    this.connectedDevicesArray.push(message)
+    this.messageSource.next(this.connectedDevicesArray);
   }
 }
